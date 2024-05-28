d9c34c43ccc770ce4f202b62f42fc18ee8ae910d
S-Orietta <150481175+S-Orietta@users.noreply.github.com>
2024-05-28 17:29:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('.peti').hover(
+        function() {
+          $('.peti-info').fadeIn(300);
+        },
+        function() {
+          $('.peti-info').fadeOut(300);
+        }
+      );
+
+      $('.chocola').hover(
+        function() {
+          $('.chocola-info').fadeIn(300);
+        },
+        function() {
+          $('.chocola-info').fadeOut(300);
+        }
+        
+      );
+});
+
